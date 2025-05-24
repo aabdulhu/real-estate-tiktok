d599d8bee2ce72eed1428b18b191473e399e6bbd
aabdulhu <61359243+aabdulhu@users.noreply.github.com>
2025-05-23 20:58:32 -0400

    first commit5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Card } from '@/components/ui/card';
+import { Card } from './components/ui/card';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
